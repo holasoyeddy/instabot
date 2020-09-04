98700355f1472dbf79a7d6b32b4bc1054b615f35
Eddy Rubio <eddyrubioc@gmail.com>
2020-09-04 18:41:14 -0500

    added authentication controller

--- src/controllers.js
+++ src/controllers.js
@@ -1,9 +1,17 @@
+const auth = require('./authentication');
+
 const hello = (req, res) => {
+    console.debug("In hello controller")
     res.send('Hello, world!');
 };
 
 const authenticate = (req, res) => {
-    res.send('Login');
+    let token = auth.authenticate(req.body.key);
+    return token == null ? res.sendStatus(401) : res.send(token);
+};
+
+const refresh = (req, res) => {
+    console.debug("REFRESHING")
 };
 
 const addPostToQueue = (req, res) => {
@@ -14,4 +22,4 @@ const getQueuedPosts = (req, res) => {
     res.send('Queued posts');
 };
 
-module.exports = { hello, authenticate, addPostToQueue, getQueuedPosts };
+module.exports = { hello, authenticate, refresh, addPostToQueue, getQueuedPosts };
