29200669c05856f2856b0587974c9773581e066a
Eddy Rubio <eddyrubioc@gmail.com>
2020-09-04 18:35:39 -0500

    updated server use

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
-const Server = require('./server');
-const Configuration = require('./configuration');
-const jsonPath = '../config/server.' + process.env.NODE_ENV + '.json';
-const json = require(jsonPath);
-
-const conf = new Configuration(json);
-const server = new Server(conf);
-
+const server = require('./server');
 server.init();
 server.run();
